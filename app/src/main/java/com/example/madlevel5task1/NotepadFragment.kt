a2eeb4858905795344d78bc8bf9893e7428b46ed
Skander <skander@bibani.nl>
2020-12-27 14:24:35 +0100

    Enable view binding

--- app/src/main/java/com/example/madlevel5task1/NotepadFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/NotepadFragment.kt
@@ -23,6 +23,6 @@ class NotepadFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
     }
 }
