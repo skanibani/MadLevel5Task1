c232a716c08e31952af08abd46d512c6aa0d1020
Skander <skander@bibani.nl>
2020-12-27 14:23:20 +0100

    Remove resource coupled code

--- app/src/main/java/com/example/madlevel5task1/NotepadFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/NotepadFragment.kt
@@ -23,9 +23,6 @@ class NotepadFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        
     }
 }
