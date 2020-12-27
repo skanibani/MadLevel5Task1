2350610ff7226dad2d03443d2a5c42c4eb6623cf
Skander <skander@bibani.nl>
2020-12-27 16:17:30 +0100

    Add viewbinding in fragment

--- app/src/main/java/com/example/madlevel5task1/NotepadFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/NotepadFragment.kt
@@ -5,24 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.madlevel5task1.databinding.FragmentNotepadBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class NotepadFragment : Fragment() {
 
+    private var _binding: FragmentNotepadBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notepad, container, false)
+        _binding = FragmentNotepadBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
