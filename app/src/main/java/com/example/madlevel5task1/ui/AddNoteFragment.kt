a1e12e8574004c887adcdbea5350a28afdbf7f96
Skander <skander@bibani.nl>
2020-12-29 21:11:13 +0100

    Add ViewModel to fragment

--- app/src/main/java/com/example/madlevel5task1/ui/AddNoteFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/ui/AddNoteFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.madlevel5task1.R
 import com.example.madlevel5task1.databinding.FragmentAddNoteBinding
+import com.example.madlevel5task1.model.NoteViewModel
 
 class AddNoteFragment : Fragment() {
 
     private var _binding: FragmentAddNoteBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: NoteViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,9 +34,29 @@ class AddNoteFragment : Fragment() {
 
         binding.btnSave.setOnClickListener {
 
+            saveNote()
+
             // Back to Notepad
             findNavController().navigate(R.id.action_AddNoteFragment_to_NotepadFragment)
         }
+
+        observeNote()
+    }
+
+    private fun observeNote() {
+        viewModel.note.observe(viewLifecycleOwner, Observer {
+            note ->
+                note?.let {
+                    binding.inputTitle?.setText(it.title)
+                    binding.inputNote?.setText(it.text)
+                }
+        })
+    }
+
+    private fun saveNote() {
+        viewModel.updateNote(
+                binding.inputTitle?.text.toString(),
+                binding.inputNote?.text.toString())
     }
 
     override fun onDestroyView() {
